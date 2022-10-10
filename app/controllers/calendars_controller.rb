84a399248c8672b9941d3dea3b27e9ff5aa1cbd6
masafumiikeda <244marshall144@gmail.com>
2022-10-10 18:33:04 +0900

    カレンダーミニアプリ完成

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,6 +1,21 @@
 class CalendarsController < ApplicationController
 
   def index
-    @caleenders = Calendar.all
+    @calenders = Calendar.all
+  end
+
+  def new
+    @calendar = Calendar.new
+  end
+
+  def create
+    Calendar.create(calendar_parameter)
+    redirect_to root_path
+  end
+
+  private
+
+  def calendar_parameter
+    params.require(:event).permit(:title, :content, :start_time)
   end
 end
